d338c91241207d36e2d5c301de34b843f67c0640
maliang <maliang@xywy.com>
2020-09-18 22:43:12 +0800

    ml

--- views/operate/search.php
+++ views/operate/search.php
@@ -9,7 +9,7 @@ use app\components\widgets\ActiveForm;
 /* @var $searchModel \app\models\search\OperateSearch */
 ?>
 <?php $form = ActiveForm::begin(['module' => ActiveForm::TYPE_SEARCH]) ?>
-<?=  Laydate::widget(['form' => $form, 'model' => $searchModel, 'label' => '日期：', 'startDate' => 'startDate', 'endDate' => 'endDate']) ?>
+<?=  Laydate::widget(['form' => $form, 'model' => $searchModel, 'label' => '操作日期', 'startDate' => 'startDate', 'endDate' => 'endDate']) ?>
 <?= $form->field($searchModel, 'type')->dropDownList(OperateRecord::typeArray(), ['prompt' => Yii::t('common', 'All')]) ?>
 <?= $form->field($searchModel, 'module')->dropDownList(OperateRecord::moduleArray(), ['prompt' => Yii::t('common', 'All')]) ?>
 <?= $form->field($searchModel, 'admin_id')->dropDownList(Admin::adminArray(), ['prompt' => Yii::t('common', 'All')]) ?>
