113a6ee057f2fce31deb78a1d2341417e2c7dc70
maliang <maliang@xywy.com>
2020-09-18 23:27:43 +0800

    ml

--- views/payment/search.php
+++ views/payment/search.php
@@ -11,8 +11,10 @@ use app\components\widgets\ActiveForm;
 <?php $form = ActiveForm::begin(['module' => ActiveForm::TYPE_SEARCH]) ?>
 <?= $form->field($searchModel, 'cust_year') ?>
 <?=  Laydate::widget(['form' => $form, 'model' => $searchModel, 'label' => '缴费日期', 'startDate' => 'startDate', 'endDate' => 'endDate']) ?>
+<br>
 <?= $form->field($searchModel, 'cust_area') ?>
 <?= $form->field($searchModel, 'cust_address') ?>
+<br>
 <?= $form->field($searchModel, 'payment_method')->dropDownList(CustPayment::$paymentMethodArray, ['prompt' => Yii::t('common', 'All')]) ?>
 <?= $form->field($searchModel, 'status_code')->dropDownList(CustPayment::$statusCodeArray, ['prompt' => Yii::t('common', 'All')]) ?>
 <?= $form->field($searchModel, 'admin_id')->dropDownList(Admin::adminArray(), ['prompt' => Yii::t('common', 'All')]) ?>
