5758b4c8d910b7cb221f9581d0985e8cd8280419
maliang <maliang@xywy.com>
2020-09-19 13:02:52 +0800

    ml

--- components/grid/ActionColumn.php
+++ components/grid/ActionColumn.php
@@ -52,7 +52,7 @@ class ActionColumn extends \yii\grid\ActionColumn
                 }
 
                 if(in_array($name, ['charge', 'not-charge', 'vacant-house', 'cut-heating'])){
-                    if($model->statusCode == CustPayment::STATUS_NOT_COLLECTED && $name == 'cut-heating'){
+                    if($model->status_code == CustPayment::STATUS_NOT_COLLECTED && $name == 'cut-heating'){
                         return '';
                     }
 
