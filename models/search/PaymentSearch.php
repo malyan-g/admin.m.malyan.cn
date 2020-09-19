26fb8a714bee2df2c2316cd86273a4cfa673c5a6
maliang <maliang@xywy.com>
2020-09-19 11:27:16 +0800

    ml

--- models/search/PaymentSearch.php
+++ models/search/PaymentSearch.php
@@ -135,6 +135,7 @@ class PaymentSearch extends CustPayment
 
         $payment = $query->select(['admin_id','payment_method','count(id) as num', 'sum(paid_amount) as paid_amount','sum(amount) as amount'])
             ->groupBy(['admin_id', 'payment_method'])
+            ->orderBy(['admin_id' => SORT_ASC])
             ->all();
 
         $data = [];
@@ -195,9 +196,8 @@ class PaymentSearch extends CustPayment
             'cust_year' => $this->cust_year
         ]);
 
-        $payment = $query->select(['admin_id','payment_method','count(id) as num', 'sum(paid_amount) as paid_amount','sum(amount) as amount'])
+        $payment = $query->select(['admin_id','payment_method', 'status_code', 'count(id) as num', 'sum(paid_amount) as paid_amount','sum(amount) as amount'])
             ->groupBy(['payment_method', 'status_code'])
-            ->orderBy(['admin_id' => SORT_ASC])
             ->all();
 
         $data = [
